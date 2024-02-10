dce8b7cf1ab7e5ce353322974f1df819c7e2393a
Maximiliano Kazanski <maxikazanski@gmail.com>
2024-02-10 16:51:12 -0300

    fix: add 'Municipio' types to 'AllTypes'

--- src/interfaces/all-params.interface.ts
+++ src/interfaces/all-params.interface.ts
@@ -1,8 +1,11 @@
 import { type ProvinciaParams } from './provincias.interface'
 import { type DepartamentoParams } from './departamentos.interface'
+import { type MunicipioParams } from './municipios.interface'
 
 export type AllParams =
       ProvinciaParams
       | ProvinciaParams[]
       | DepartamentoParams
       | DepartamentoParams[]
+      | MunicipioParams
+      | MunicipioParams[]
